cff09d5d2933f69eb09c804afd310aefacb89eb8
Felipe Anta <felipeantauct@gmail.com>
2023-05-13 04:41:57 -0300

    Entrega final

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,52 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useCartContext } from '../../context/CartContext'
 import './CartContainer.css'
 import { Link } from 'react-router-dom'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
 import Formulario from '../Formulario/Formulario'
 
 const CartContainer = () => {
-
-  // const [dataForm, setDataForm] = useState({
-  //   name: '',
-  //   phone: '',
-  //   email: '',
-  //   mensaje: ''
-  // })
   const { cartList, vaciarCarrito, totalPrice, removeFromCart, decrementProduct, incrementProduct } = useCartContext()
-
-  // const generarOrden = (evt) => {
-  //   evt.preventDefault()
-
-  //   const order = {}
-  //   order.buyer = dataForm
-  //   order.items = cartList.map(({ name, id, price, cantidad }) => ({ id, name, price, cantidad }))
-  //   order.total = totalPrice;
-
-  //   const dbFirestore = getFirestore()
-  //   const ordersCollection = collection(dbFirestore, 'orders')
-
-  //   addDoc(ordersCollection, order)
-  //     .then(resp => console.log(resp))
-    // //actualizar update
-    // const queryDoc = doc(dbFirestore, 'productos', '0n7Y6vcO9tRjcgG5QdUm')
-    // updateDoc(queryDoc, {stock: 120})
-    // .finally(()=> 'finalizo la actualizacion')
-
-    // //borrado logico
-    // updateDoc(queryDoc, {isActive: false})
-    // .finally(()=> 'finalizo la actualizacion')
-  // }
-  // const handleOnChange = (evt) => {
-  //   console.log('nombre del input', evt.target.name)
-  //   console.log('nombre del valor ', evt.target.value)
-  //   setDataForm({
-  //     ...dataForm,
-  //     [evt.target.name]: evt.target.value
-  //   })
-  // }
-  // console.log(dataForm)
-
   if (cartList.length === 0 && totalPrice === 0) {
     return (
       <div className='cart-vacio'>
@@ -93,55 +52,6 @@ const CartContainer = () => {
         <button onClick={vaciarCarrito} className='btn btn-outline-danger'>Vaciar Carrito</button>
         <Link to={'/'} className="btn btn-outline-secondary">Volver al listado</Link>
       </div>
-
-      {/* <form onSubmit={generarOrden} className="form-container">
-        <div className="form-section">
-          <label htmlFor="name">Nombre:</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="name"
-            value={dataForm.name}
-            placeholder="Ingrese el nombre"
-          />
-        </div>
-
-        <div className="form-section">
-          <label htmlFor="phone">Teléfono:</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="phone"
-            value={dataForm.phone}
-            placeholder="Ingrese el número"
-          />
-        </div>
-
-        <div className="form-section">
-          <label htmlFor="email">Email:</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="email"
-            value={dataForm.email}
-            placeholder="Ingrese el email"
-          />
-        </div>
-
-        <div className="form-section">
-          <label htmlFor="mensaje">Mensaje:</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="mensaje"
-            value={dataForm.mensaje}
-            placeholder="Ingrese el mensaje"
-          />
-        </div>
-
-        <button className="btn btn-outline-success submit-button">Generar orden</button>
-      </form> */}
-
       <Formulario cartList={cartList} totalPrice={totalPrice}/>
     </div>
   )
--- src/components/Formulario/Form.jsx
+++ src/components/Formulario/Form.jsx
@@ -1,6 +1,8 @@
-import './Form'
+import { useState } from 'react';
+import './Form.css'
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
 const Form = ({formData, handleOnChange, errors, validateForm, setFormData, cartList, totalPrice}) => {
+  const [orderPlaced, setOrderPlaced] = useState(false);
   const handleOnSubmit = async (evt) => {
     evt.preventDefault();
     if (validateForm()) {
@@ -23,6 +25,8 @@ const Form = ({formData, handleOnChange, errors, validateForm, setFormData, cart
           phone: '',
           message: '',
         });
+
+        setOrderPlaced(true);
       } catch (error) {
         console.error('Error al generar la orden:', error);
       }
@@ -30,35 +34,28 @@ const Form = ({formData, handleOnChange, errors, validateForm, setFormData, cart
   };
     return (
       <div>
+        {orderPlaced && <p className="order-success">Orden exitosa</p>}
         <form onSubmit={handleOnSubmit} className="form-container">
           <div className="form-section">
             <label htmlFor="name">Nombre:</label>
             <input type="text" name="name" placeholder='ingrese el nombre' onChange={handleOnChange} value={formData.name}/>
-            <br />
             {errors && errors.name && <span>{errors.name}</span>}
-            <br />
+          
           </div>
           <div className="form-section">
             <label htmlFor="email">Email:</label>
             <input type="text" name="email" placeholder='ingrese el email' onChange={handleOnChange} value={formData.email}/>
-            <br />
             {errors && errors.email && <span>{errors.email}</span>}
-            <br />
           </div>
           <div className="form-section">
             <label htmlFor="phone">Teléfono:</label>
             <input type="text" name="phone" placeholder='ingrese el telefono' onChange={handleOnChange} value={formData.phone}/>
-            <br />
             {errors && errors.phone && <span>{errors.phone}</span>}
-            <br />
           </div>
           <div className="form-section">
-            <br/>
             <label htmlFor="mensaje">Mensaje:</label>
-            <input type="text" name="message" placeholder='Deje un mensaje' onChange={handleOnChange} value={formData.message}/>
-            <br />
+            <textarea type="text" name="message" placeholder='Deje un mensaje (opcional)' onChange={handleOnChange} value={formData.message} rows="5" cols="30"/>
             {errors && errors.message && <span>{errors.message}</span>}
-            <br />
           </div>
             <button className="btn btn-outline-success submit-button">Generar orden</button>
         </form>
