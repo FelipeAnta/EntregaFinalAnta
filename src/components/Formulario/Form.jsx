331de69bddbc90432245a08514d975c7f87da3c9
Felipe Anta <felipeantauct@gmail.com>
2023-05-15 16:52:27 -0300

    final commit

--- src/components/Formulario/Form.jsx
+++ src/components/Formulario/Form.jsx
@@ -1,66 +1,75 @@
 import { useState } from 'react';
 import './Form.css'
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
-const Form = ({formData, handleOnChange, errors, validateForm, setFormData, cartList, totalPrice}) => {
+import { Link } from 'react-router-dom';
+import { useCartContext } from '../../context/CartContext';
+const Form = ({ formData, handleOnChange, errors, validateForm, setFormData, cartList, totalPrice }) => {
   const [orderPlaced, setOrderPlaced] = useState(false);
+  const {vaciarCarrito} = useCartContext()
   const handleOnSubmit = async (evt) => {
     evt.preventDefault();
     if (validateForm()) {
-      try {
-        const order = {}
-        order.buyer =formData
-        order.items = cartList.map(({ name, id, price, cantidad }) => ({ id, name, price, cantidad }))
-        order.total = totalPrice;
-  
+        const order = {
+          buyer: formData,
+          items: cartList.map(({ name, id, price, cantidad }) => ({ id, name, price, cantidad })),
+          total: totalPrice
+        };
+
         const dbFirestore = getFirestore();
         const ordersCollection = collection(dbFirestore, 'orders');
-  
+
         const docRef = await addDoc(ordersCollection, order);
         console.log('Orden generada con ID:', docRef.id);
-        
-        // Limpia los campos del formulario
+
         setFormData({
           name: '',
           email: '',
           phone: '',
-          message: '',
+          message: ''
         });
-
         setOrderPlaced(true);
-      } catch (error) {
-        console.error('Error al generar la orden:', error);
       }
-    }
   };
-    return (
-      <div>
-        {orderPlaced && <p className="order-success">Orden exitosa</p>}
+
+  const handleBackToList = () => {
+    setOrderPlaced(false);
+    vaciarCarrito();
+  };
+
+  return (
+    <div>
+      {orderPlaced ? (
+        <div className='order-section'>
+          <p className="order-success">Orden exitosa</p>
+          <Link to={'/'} className="btn btn-outline-success" onClick={handleBackToList}>Hacer otra compra</Link>
+        </div>
+      ) : (
         <form onSubmit={handleOnSubmit} className="form-container">
           <div className="form-section">
             <label htmlFor="name">Nombre:</label>
-            <input type="text" name="name" placeholder='ingrese el nombre' onChange={handleOnChange} value={formData.name}/>
+            <input type="text" name="name" placeholder="Ingrese el nombre" onChange={handleOnChange} value={formData.name} />
             {errors && errors.name && <span>{errors.name}</span>}
-          
           </div>
           <div className="form-section">
             <label htmlFor="email">Email:</label>
-            <input type="text" name="email" placeholder='ingrese el email' onChange={handleOnChange} value={formData.email}/>
+            <input type="text" name="email" placeholder="Ingrese el email" onChange={handleOnChange} value={formData.email} />
             {errors && errors.email && <span>{errors.email}</span>}
           </div>
           <div className="form-section">
             <label htmlFor="phone">Teléfono:</label>
-            <input type="text" name="phone" placeholder='ingrese el telefono' onChange={handleOnChange} value={formData.phone}/>
+            <input type="text" name="phone" placeholder="Ingrese el teléfono" onChange={handleOnChange} value={formData.phone} />
             {errors && errors.phone && <span>{errors.phone}</span>}
           </div>
           <div className="form-section">
             <label htmlFor="mensaje">Mensaje:</label>
-            <textarea type="text" name="message" placeholder='Deje un mensaje (opcional)' onChange={handleOnChange} value={formData.message} rows="5" cols="30"/>
+            <textarea type="text" name="message" placeholder="Deje un mensaje (opcional)" onChange={handleOnChange} value={formData.message} rows="5" cols="30" />
             {errors && errors.message && <span>{errors.message}</span>}
           </div>
-            <button className="btn btn-outline-success submit-button">Generar orden</button>
+          <button className="btn btn-outline-success submit-button">Generar orden</button>
         </form>
-      </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default Form
+export default Form;
